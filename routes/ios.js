6d16c2a70eb2dc78f99644515e70a85bb3e83743
CenoX <admin@cenox.co>
2018-01-31 21:41:29 +0900

    writing validation middleware

--- routes/ios.js
+++ routes/ios.js
@@ -6,8 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'iOS' });
 });
     
-router.post('/validation', function (req, res) {
-    res.send('POST request to homepage');
+router.all('/validation', function (req, res) {
+
+  let isSandbox = req.query.sandbox ? true : false
+
+  if (req.method == 'GET') {
+    res.status(500).json({
+      error: "No get request!",
+      isSandbox: isSandbox
+    })
+    return;
+  }
+
+  console.log(req.body) 
+  console.log(req.headers)  
+  if (req.get('x-overrapid-version') == undefined) {
+    res.status(500).json({
+      error: "This client is not OverRapid client!",
+      isSandbox: isSandbox
+    })
+  } else {
+    res.send('Proceed');
+  }
 });
 
 module.exports = router;
